60984bb550e6970a5f88756f574f8a0959c3f0e5
Angelina <CatMintFox@gmail.com>
2020-12-02 20:50:32 +0500

    g

---
+++ L2/L2/Message.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Net;
+using System.Windows;
+using Path = System.IO.Path;
+
+namespace L2
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : Window
+    {
+        public Message()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient myWebClient = new WebClient();
+            string myStringWebResource = @"https://bdu.fstec.ru/files/documents/thrlist.xlsx";
+
+            myWebClient.DownloadFile(myStringWebResource, MainWindow.fileName);
+            MainWindow.filepath = Path.GetFullPath(MainWindow.fileName);
+            File.Move(MainWindow.filepath, textBox1.Text + MainWindow.fileName);
+            MainWindow.filepath = Path.GetFullPath(MainWindow.fileName);
+            this.Close();
+        }
+    }
+}
